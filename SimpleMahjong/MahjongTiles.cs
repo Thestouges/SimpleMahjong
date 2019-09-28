79cab905a8fec9810f0025c5ba8d981bf5ee1d7b
Thestouges <thestouges@gmail.com>
2019-09-27 20:17:06 -0400

    Added initialization of tiles

---
+++ SimpleMahjong/MahjongTiles.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleMahjong
+{
+    public class MahjongTiles
+    {
+        List<Tile> MahjongSet;
+
+        MahjongTiles()
+        {
+            InitializeSet();
+        }
+
+        private void InitializeSet()
+        {
+            AddWindTile();
+            AddDragonTile();
+        }
+
+        private void AddWindTile()
+        {
+            Tile WindTile = new Tile();
+            WindTile.Suit = Suit.Wind;
+            WindTile.Terminal = true;
+
+            WindTile.Letter = "EAST";
+            MahjongSet.Add(WindTile);
+
+            WindTile.Letter = "SOUTH";
+            MahjongSet.Add(WindTile);
+
+            WindTile.Letter = "NORTH";
+            MahjongSet.Add(WindTile);
+
+            WindTile.Letter = "SOUTH";
+            MahjongSet.Add(WindTile);
+        }
+
+        private void AddDragonTile()
+        {
+            Tile DragonTile = new Tile();
+            DragonTile.Suit = Suit.Dragon;
+            DragonTile.Terminal = true;
+
+            DragonTile.Letter = "RED";
+            MahjongSet.Add(DragonTile);
+
+            DragonTile.Letter = "GREEN";
+            MahjongSet.Add(DragonTile);
+
+            DragonTile.Letter = "WHITE";
+            MahjongSet.Add(DragonTile);
+        }
+
+        private void AddNumberTile()
+        {
+            for (int j = 1; j <= 3; j++)
+            {
+                Suit suit = new Suit();
+                suit = (Suit)j;
+                for (int i = 1; i <= 9; i++)
+                {
+                    Tile NumberTile = new Tile();
+                    NumberTile.Suit = suit;
+
+                    if (i == 1 || i == 9)
+                    {
+                        NumberTile.Terminal = true;
+                    }
+                    else
+                    {
+                        NumberTile.Terminal = false;
+                    }
+
+                    NumberTile.Letter = i.ToString();
+                    MahjongSet.Add(NumberTile);
+                }
+            }
+        }
+    }
+}
---
+++ SimpleMahjong/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleMahjong
+{
+    public enum Suit
+    {
+        Bamboo = 1,
+        Dots = 2,
+        Character = 3,
+        Wind = 4,
+        Dragon = 5
+    }
+    public class Tile
+    {
+        public Suit Suit;
+        public string Letter;
+        public bool Terminal;
+    }
+}
