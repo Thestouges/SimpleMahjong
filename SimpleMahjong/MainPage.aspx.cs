9574256d07fd2020e79bf3199374def596d28292
Thestouges <thestouges@gmail.com>
2019-10-01 01:28:10 -0400

    edited path for tiles

--- SimpleMahjong/MainPage.aspx.cs
+++ SimpleMahjong/MainPage.aspx.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.HtmlControls;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Web.Script.Serialization;
+
 
 namespace SimpleMahjong
 {
@@ -13,267 +18,227 @@ namespace SimpleMahjong
         List<Tile> hand;
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!Page.IsPostBack)
+            MahjongFunctions = new MahjongFunctions();
+            hand = new List<Tile>();
+            if (Page.IsPostBack)
             {
-                MahjongFunctions = new MahjongFunctions();
-                hand = new List<Tile>();
+                
             }
             else
             {
-                ShowHand();
+                Session.Clear();
             }
-           
+
         }
 
         private void ShowHand() {
+            if (Session["hand"] != null)
+            {
+                hand = (List<Tile>)Session["hand"];
+            }
+            else
+            {
+                return;
+            }
+
             SortHand();
+            string html = "";
             foreach (Tile item in hand)
             {
-                handID.InnerHtml += "<asp:ImageButton Class=\"tile\" ImageUrl=\"" + item.GetImagePath();
+                html += "<input type=Image class=myhand src=" + item.GetImagePath() + ">";
             }
+            handID.InnerHtml = html;
         }
 
         private void SortHand()
         {
-            hand.OrderBy(x => x.Number);
+            if (hand.Count > 0)
+            {
+                hand.OrderBy(x => x.Number);
+            }
         }
-        
+
+        private void InsertToHand(int num)
+        {
+            if (Session["hand"] != null)
+            {
+                hand = (List<Tile>)Session["hand"];
+            }
+            Tile tile = new Tile();
+            tile.Number = num;
+            hand.Add(tile);
+            Session["hand"] = hand;
+            ShowHand();
+        }
+
         protected void btnBamboo1_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 1;
-            hand.Add(tile);
+            InsertToHand(1);
         }
 
         protected void btnBamboo2_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 2;
-            hand.Add(tile);
+            InsertToHand(2);
         }
 
         protected void btnBamboo3_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 3;
-            hand.Add(tile);
+            InsertToHand(3);
         }
 
         protected void btnBamboo4_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 4;
-            hand.Add(tile);
+            InsertToHand(4);
         }
 
         protected void btnBamboo5_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 5;
-            hand.Add(tile);
+            InsertToHand(5);
         }
 
         protected void btnBamboo6_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 6;
-            hand.Add(tile);
+            InsertToHand(6);
         }
 
         protected void btnBamboo7_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 7;
-            hand.Add(tile);
+            InsertToHand(7);
         }
 
         protected void btnBamboo8_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 8;
-            hand.Add(tile);
+            InsertToHand(8);
         }
 
         protected void btnBamboo9_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 9;
-            hand.Add(tile);
+            InsertToHand(9);
         }
 
         protected void btnPin1_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 11;
-            hand.Add(tile);
+            InsertToHand(11);
         }
 
         protected void btnPin2_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 12;
-            hand.Add(tile);
+            InsertToHand(12);
         }
 
         protected void btnPin3_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 13;
-            hand.Add(tile);
+            InsertToHand(13);
         }
 
         protected void btnPin4_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 14;
-            hand.Add(tile);
+            InsertToHand(14);
         }
 
         protected void btnPin5_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 15;
-            hand.Add(tile);
+            InsertToHand(15);
         }
 
         protected void btnPin6_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 16;
-            hand.Add(tile);
+            InsertToHand(16);
         }
 
         protected void btnPin7_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 17;
-            hand.Add(tile);
+            InsertToHand(17);
         }
 
         protected void btnPin8_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 18;
-            hand.Add(tile);
+            InsertToHand(18);
         }
 
         protected void btnPin9_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 19;
-            hand.Add(tile);
+            InsertToHand(19);
         }
 
         protected void btnMan1_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 21;
-            hand.Add(tile);
+            InsertToHand(21);
         }
 
         protected void btnMan2_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 22;
-            hand.Add(tile);
+            InsertToHand(22);
         }
 
         protected void btnMan3_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 23;
-            hand.Add(tile);
+            InsertToHand(23);
         }
 
         protected void btnMan4_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 24;
-            hand.Add(tile);
+            InsertToHand(24);
         }
 
         protected void btnMan5_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 25;
-            hand.Add(tile);
+            InsertToHand(25);
         }
 
         protected void btnMan6_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 26;
-            hand.Add(tile);
+            InsertToHand(26);
         }
 
         protected void btnMan7_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 27;
-            hand.Add(tile);
+            InsertToHand(27);
         }
 
         protected void btnMan8_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 28;
-            hand.Add(tile);
+            InsertToHand(28);
         }
 
         protected void btnMan9_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 29;
-            hand.Add(tile);
+            InsertToHand(29);
         }
 
         protected void btnEast_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 32;
-            hand.Add(tile);
+            InsertToHand(32);
         }
 
         protected void btnSouth_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 33;
-            hand.Add(tile);
+            InsertToHand(33);
         }
 
         protected void btnWest_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 34;
-            hand.Add(tile);
+            InsertToHand(34);
         }
 
         protected void btnNorth_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 35;
-            hand.Add(tile);
+            InsertToHand(35);
         }
 
         protected void btnRed_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 36;
-            hand.Add(tile);
+            InsertToHand(36);
         }
 
         protected void White_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 37;
-            hand.Add(tile);
+            InsertToHand(37);
         }
 
         protected void Green_Click(object sender, ImageClickEventArgs e)
         {
-            Tile tile = new Tile();
-            tile.Number = 38;
-            hand.Add(tile);
+            InsertToHand(38);
         }
     }
 }
